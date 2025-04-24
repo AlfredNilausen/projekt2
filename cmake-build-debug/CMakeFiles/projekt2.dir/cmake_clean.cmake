74fdd2ae592211aa44e95194a9c93c9ad4231e8a
Flace333 <magnusfehrmann@gmail.com>
2025-04-24 12:59:28 +0200

    Made card.c, card.h, columns.c, columns.h

---
+++ cmake-build-debug/CMakeFiles/projekt2.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projekt2.dir/Controller/main.c.o"
+  "CMakeFiles/projekt2.dir/Controller/main.c.o.d"
+  "CMakeFiles/projekt2.dir/Model/card.c.o"
+  "CMakeFiles/projekt2.dir/Model/card.c.o.d"
+  "CMakeFiles/projekt2.dir/Model/columns.c.o"
+  "CMakeFiles/projekt2.dir/Model/columns.c.o.d"
+  "projekt2"
+  "projekt2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/projekt2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
