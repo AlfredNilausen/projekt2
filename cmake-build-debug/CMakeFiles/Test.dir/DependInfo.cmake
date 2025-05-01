0e41a4e153c92496c17791fd81d7c071a7fed617
deni1526 <s246021@student.dtu.dk>
2025-05-01 13:41:46 +0200

    Q = quit game QQ og P while playing

---
+++ cmake-build-debug/CMakeFiles/Test.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Bruger/CLionProjects/projekt2/Model/card.c" "CMakeFiles/Test.dir/Model/card.c.obj" "gcc" "CMakeFiles/Test.dir/Model/card.c.obj.d"
+  "C:/Users/Bruger/CLionProjects/projekt2/Model/columns.c" "CMakeFiles/Test.dir/Model/columns.c.obj" "gcc" "CMakeFiles/Test.dir/Model/columns.c.obj.d"
+  "C:/Users/Bruger/CLionProjects/projekt2/Model/deck.c" "CMakeFiles/Test.dir/Model/deck.c.obj" "gcc" "CMakeFiles/Test.dir/Model/deck.c.obj.d"
+  "C:/Users/Bruger/CLionProjects/projekt2/test/test_deck.c" "CMakeFiles/Test.dir/test/test_deck.c.obj" "gcc" "CMakeFiles/Test.dir/test/test_deck.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
